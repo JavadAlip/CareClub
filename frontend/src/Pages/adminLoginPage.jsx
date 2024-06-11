02f9baf1cd47b4ba7631a66e5118ee069b9ee6a7
JavadAlip <javadalipp911@gmail.com>
2024-06-11 17:32:59 +0530

    Add private route for data secure

--- frontend/src/Pages/adminLoginPage.jsx
+++ frontend/src/Pages/adminLoginPage.jsx
@@ -1,64 +1,138 @@
+// import React, { useState } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { adminLogin } from '../Redux/Actions/adminActions';
+// import AdminDashboard from '../Pages/adminDashBoard';
+
+// const AdminLogin = () => {
+//   const [username, setUsername] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [isAuthenticated, setIsAuthenticated] = useState(false);
+//   const [error, setError] = useState(null);
+//   const dispatch = useDispatch();
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     try {
+//       await dispatch(adminLogin({ username, password }));
+//       setIsAuthenticated(true);
+//       console.log('Login successful');
+//     } catch (error) {
+//       console.error('Login failed:', error);
+//       if (error.response) {
+//         setError('Network error. Please check your internet connection.');
+//       } else {
+//         setError('Login failed. Please check your username and password.');
+//       }
+//     }
+//   };
+
+//   // If the admin is authenticated, render the AdminDashboard
+//   if (isAuthenticated) {
+//     return <AdminDashboard />;
+//   }
+ 
+//   return (
+//     <section className=' pt-[160px]'>
+//     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+//       <div className='w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10 items-center'>
+//         <div className="login-form" style={{ maxWidth: '400px', width: '100%' }}>
+//           <h1 style={{ textAlign: 'center', marginBottom: '20px', }}>Admin Login</h1>
+//           <form onSubmit={handleSubmit} style={{ textAlign: 'center' }}> 
+//             <input
+//               type="text"
+//               placeholder="Username"
+//               value={username}
+//               onChange={(e) => setUsername(e.target.value)}
+//               required
+//               className="bg-[#7fad80] px-4 py-2 rounded-md mb-4"
+//               style={{ width: '100%' }}
+//             />
+//             <input
+//               type="password"
+//               placeholder="Password"
+//               value={password}
+//               onChange={(e) => setPassword(e.target.value)}
+//               required
+//               className="bg-[#7fad80] px-4 py-2 rounded-md mb-4"
+//               style={{ width: '100%' }}
+//             />
+//             <button type="submit" className="text-white px-4 btn py-2 rounded-md" style={{ width: '100%' }}>Login</button>
+//             {error && <div className="text-green mt-2">{error}</div>}
+//           </form>
+//         </div>
+//       </div>
+//     </div>
+//     </section>
+
+
+//   );
+// };
+
+// export default AdminLogin;
+
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { adminLogin } from '../Redux/Actions/adminActions';
 import AdminDashboard from '../Pages/adminDashBoard';
 
 const AdminLogin = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [error, setError] = useState(null);
   const dispatch = useDispatch();
+  const isAuthenticated = useSelector(state => state.admin.isAuthenticated);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       await dispatch(adminLogin({ username, password }));
-      setIsAuthenticated(true);
       console.log('Login successful');
     } catch (error) {
       console.error('Login failed:', error);
       if (error.response) {
-        // If the error has a response object, it means it's a server error
-        setError('Login failed. Please check your username and password.');
-      } else {
-        // Otherwise, it's likely a network error
         setError('Network error. Please check your internet connection.');
+      } else {
+        setError('Login failed. Please check your username and password.');
       }
     }
   };
-  
 
-  // If the admin is authenticated, render the AdminDashboard
   if (isAuthenticated) {
     return <AdminDashboard />;
   }
 
   return (
-    <div className="login-form bg-red-500" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <form onSubmit={handleSubmit} style={{ width: '100%', maxWidth: '400px' }}>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-          className="bg-yellow-300 px-4 py-2 rounded-md mb-4"
-          style={{ width: '100%' }}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          className="bg-yellow-300 px-4 py-2 rounded-md mb-4"
-          style={{ width: '100%' }}
-        />
-        <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md">Login</button>
-        {error && <div className="text-white mt-2">{error}</div>}
-      </form>
-    </div>
+    <section className='pt-[160px]'>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <div className='w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10 items-center'>
+          <div className="login-form" style={{ maxWidth: '400px', width: '100%' }}>
+            <h1 style={{ textAlign: 'center', marginBottom: '20px' }}>Admin Login</h1>
+            <form onSubmit={handleSubmit} style={{ textAlign: 'center' }}>
+              <input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+                className="bg-[#7fad80] px-4 py-2 rounded-md mb-4"
+                style={{ width: '100%' }}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                className="bg-[#7fad80] px-4 py-2 rounded-md mb-4"
+                style={{ width: '100%' }}
+              />
+              <button type="submit" className="text-white px-4 btn py-2 rounded-md" style={{ width: '100%' }}>Login</button>
+              {error && <div className="text-green mt-2">{error}</div>}
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- frontend/src/Routes/routes.jsx
+++ frontend/src/Routes/routes.jsx
@@ -1,23 +1,66 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom'; 
+// import React from 'react';
+// import { Routes, Route } from 'react-router-dom'; 
 
-// user
+// // user
+// import Home from '../Pages/Home';
+// import Event from '../Pages/EventPage';
+// import About from '../Components/About/AboutMore';
+// import Contact from '../Pages/Contact';
+
+
+// // Admin
+// import AdminLogin from '../Pages/adminLoginPage';
+// import AdminDashboard from '../Pages/adminDashBoard';
+// import EventList from '../Components/Admin/AdminEventForm';
+
+// const Routers = () => {
+//   return (
+//     <Routes>
+//       <Route path='/' element={<Home />} />
+//       <Route path='/events' element={<Event />} />
+//       <Route path='/about' element={<About />} />
+//       <Route path='/contact' element={<Contact />} />
+
+//       <Route path="/admin/login" element={<AdminLogin />} />
+//       <Route path="/admin/dashboard" element={<AdminDashboard />} />
+//       <Route path="/admin/eventlist" element={<EventList />} />
+//     </Routes>
+//   );
+// };
+
+// export default Routers;
+
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+// User
 import Home from '../Pages/Home';
 import Event from '../Pages/EventPage';
+import About from '../Components/About/AboutMore';
+import Contact from '../Pages/Contact';
 
 // Admin
 import AdminLogin from '../Pages/adminLoginPage';
 import AdminDashboard from '../Pages/adminDashBoard';
 import EventList from '../Components/Admin/AdminEventForm';
 
+// Private Route
+import PrivateRoute from '../Components/Admin/PrivateRoute';
+
 const Routers = () => {
+  const isAuthenticated = useSelector(state => state.admin.isAuthenticated);
+
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/events' element={<Event />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/contact' element={<Contact />} />
+
       <Route path="/admin/login" element={<AdminLogin />} />
-      <Route path="/admin/dashboard" element={<AdminDashboard />} />
-      <Route path="/admin/eventlist" element={<EventList />} />
+      <Route path="/admin/dashboard" element={<PrivateRoute isAuthenticated={isAuthenticated} element={<AdminDashboard />} />} />
+      <Route path="/admin/eventlist" element={<PrivateRoute isAuthenticated={isAuthenticated} element={<EventList />} />} />
     </Routes>
   );
 };
