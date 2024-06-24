8f2fc2325a0007b8d6a3863eae14f726d1dfef5e
JavadAlip <javadalipp911@gmail.com>
2024-06-24 23:26:31 +0530

    Header and footer updated

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 const Footer = () => {
   return (
     <footer className='pb-10 pt-5' >
@@ -8,7 +7,7 @@ const Footer = () => {
         <div className='flex justify-between flex-col flex-wrap gap-[30px]' >
           <div>
             <p className='text-[14px] leading-6  font-semibold text-textColor text-center'>
-              © 2023 Designed by <a href="https://github.com/JavadAlip" target="_blank" rel="noopener noreferrer">JAVAD</a> All rights reserved.
+              © 2024 Designed by <a href="https://github.com/JavadAlip" target="_blank" rel="noopener noreferrer">JAVAD ALI,</a> All rights reserved.
             </p>
           </div>
         </div>
