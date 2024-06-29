3e7afed661d609c51819e6c8f811c6e1c018e893
Javad Ali P <122251720+JavadAlip@users.noreply.github.com>
2024-06-29 16:29:06 +0530

    Update Admin.js Controllers 'c' change to smalle case.

--- backend/Routes/Admin.js
+++ backend/Routes/Admin.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const { adminSignin } = require('../controllers/adminController');
+const { adminSignin } = require('../Controllers/adminController');
 router.post('/login', adminSignin);
 module.exports = router;
