3b54c57546f2a70de3162807edfd52abcab79e57
JavadAlip <javadalipp911@gmail.com>
2024-06-29 22:16:37 +0530

    new change admin js

--- backend/Routes/Admin.js
+++ backend/Routes/Admin.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
-const { adminSignin } = require('../controllers/AdminController');
-=======
+
 const { adminSignin } = require('../Controllers/AdminController');
->>>>>>> f269559e2d4aafe21f8cc6d145631457e3e5a021
 router.post('/login', adminSignin);
 module.exports = router;
