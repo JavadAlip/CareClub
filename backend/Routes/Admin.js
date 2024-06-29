ffde228cded5bed5e95ebae4c4537856d840bf4c
Javad Ali P <122251720+JavadAlip@users.noreply.github.com>
2024-06-29 17:05:29 +0530

    Update Admin.js a to A

--- backend/Routes/Admin.js
+++ backend/Routes/Admin.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const { adminSignin } = require('../Controllers/adminController');
+const { adminSignin } = require('../Controllers/AdminController');
 router.post('/login', adminSignin);
 module.exports = router;
